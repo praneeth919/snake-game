e9ce7a32834a9b64ba64197b52959ea0eaefc63c
praneeth919@gmail.com <praneeth919@gmail.com>
2018-06-12 10:23:12 +0530

    added

--- Menu.gd
+++ Menu.gd
@@ -1,20 +1,26 @@
 extends Control
 
 
+<<<<<<< HEAD
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
+=======
+>>>>>>> 14a3e92 (added)
 func _ready():
 	$VBoxContainer/Start.grab_focus()
 
 
+<<<<<<< HEAD
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+=======
+>>>>>>> 14a3e92 (added)
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://Snake.tscn")
